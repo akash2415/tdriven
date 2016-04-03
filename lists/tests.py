66f3dc248e047fb982e2f9ee529717b5b0a57d0e
karthi6624 <karthi6624@gmail.com>
2016-04-03 12:18:39 +0530

    Able to enter second list item but FT needs to be improved

--- lists/tests.py
+++ lists/tests.py
@@ -16,7 +16,7 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest
+        request = HttpRequest()
         response = home_page(request)
         # self.assertTrue(response.content.startswith(b'<html>'))
         # self.assertIn(b'<title>To-Do lists</title>', response.content)
@@ -24,4 +24,18 @@ class HomePageTest(TestCase):
         # print('response')
         # print(repr(response.content))
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+                {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
