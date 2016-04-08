db7bf29cbd5c2eccc14c70036019fe10fd1765e6
karthi6624 <karthi6624@gmail.com>
2016-04-08 17:32:01 +0530

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -55,6 +55,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        print("passed until here --------------->", edith_list_url)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
